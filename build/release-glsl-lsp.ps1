18586b06955bda46c2a461c6ae5152ab2978ee43
James Friend <james@kitefire.com>
2024-11-23 18:33:48 -0500

    fix gh action publish-glsl-lsp

--- build/release-glsl-lsp.ps1
+++ build/release-glsl-lsp.ps1
@@ -1,6 +1,6 @@
 ﻿param (
 	[string]
-	[ValidateSet("x86_64-pc-windows-msvc", "x86_64-unknown-linux-gnu", "x86_64-apple-darwin")]
+	[ValidateSet("x86_64-pc-windows-msvc", "x86_64-unknown-linux-gnu", "x86_64-apple-darwin", "aarch64-apple-darwin")]
 	$Target
 )
 
